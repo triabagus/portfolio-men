eb01dcb458bc00ed6aa9786d5d001e57bafd7f94
triabagus <triatop9@gmail.com>
2023-08-14 22:42:24 +0700

    Navbar: Add shadow header

--- assets/js/main.js
+++ assets/js/main.js
@@ -31,7 +31,14 @@ const linkAction = () => {
 navLink.forEach((n) => n.addEventListener("click", linkAction));
 
 /*=============== SHADOW HEADER ===============*/
-
+const shadowHeader = () =>{
+  const header = document.getElementById('header')
+  // When the scroll is greater than 50 viewport height, add the class list
+  this.scrollY >= 50
+    ? header.classList.add("shadow-header")
+    : header.classList.remove("shadow-header");
+}
+window.addEventListener('scroll', shadowHeader)
 
 /*=============== EMAIL JS ===============*/
 
