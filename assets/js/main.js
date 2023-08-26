f6c222ea0e4e42f5fa4905eb181ce949ba6de5bf
triabagus <triatop9@gmail.com>
2023-08-26 18:09:48 +0700

    Email sent with emailJs

--- assets/js/main.js
+++ assets/js/main.js
@@ -41,7 +41,36 @@ const shadowHeader = () =>{
 window.addEventListener('scroll', shadowHeader)
 
 /*=============== EMAIL JS ===============*/
+const contactForm = document.getElementById('contact-form'),
+      contactMessage = document.getElementById('contact-message')
 
+const sendEmail = (e) => {
+  e.preventDefault()
+
+  // serviceID - templateID - #form - publicKey
+  emailjs.sendForm(
+    "service_2qflbgb",
+    "template_e0g26e8",
+    "#contact-form",
+    "mIexuKdXkVVeH9qFT"
+  ).then(() => {
+    // Show sent message
+    contactMessage.textContent = 'Message sent successfully'
+
+    // Remove message after five seconds
+    setTimeout(() => {
+      contactMessage.textContent = ''
+    }, 5000)
+
+    // Clear input fields
+    contactForm.reset()
+  }, () =>{
+    // Show error message
+    contactMessage.textContent = 'Message not sent (service error)'
+  });
+}
+
+contactForm.addEventListener('submit', sendEmail)
 
 /*=============== SHOW SCROLL UP ===============*/ 
 
