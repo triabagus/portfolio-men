4081e55bdd70512157094601ab0ccb2d042e6f09
triabagus <triatop9@gmail.com>
2023-08-28 19:28:49 +0700

    Scroll bar up

--- assets/js/main.js
+++ assets/js/main.js
@@ -73,7 +73,13 @@ const sendEmail = (e) => {
 contactForm.addEventListener('submit', sendEmail)
 
 /*=============== SHOW SCROLL UP ===============*/ 
-
+const scrollUp = () => {
+  const scrollUp = document.getElementById('scroll-up')
+  // When the scroll is higher than 350 viewport height, add the class
+  this.scrollY >= 350 ? scrollUp.classList.add('show-scroll')
+                      : scrollUp.classList.remove('show-scroll') 
+}
+window.addEventListener('scroll', scrollUp)
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
 
