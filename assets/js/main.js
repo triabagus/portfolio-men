0b67603a54cb2b54b60528aec8fad1a338dd925b
triabagus <triatop9@gmail.com>
2023-09-03 12:49:49 +0700

    ScrollReveal Animation

--- assets/js/main.js
+++ assets/js/main.js
@@ -142,3 +142,14 @@ themeButton.addEventListener("click", () => {
 });
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
+const sr = ScrollReveal({
+  origin: 'top',
+  distance: '60px',
+  duration: 2500,
+  delay: 400,
+  // reset: true // Animation repeat
+})
+
+sr.reveal(`.home__perfil, .about__image, .contact__mail`, {origin: 'right'})
+sr.reveal(`.home__name, .home__info, .about__container, .section__title-1, .about__info, .contact__social, .contact__data`, {origin: 'left'})
+sr.reveal(`.services__card, .projects__card`, {interval: 100})
